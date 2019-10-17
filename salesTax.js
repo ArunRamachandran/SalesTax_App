40833a0dc83363d0ae8f0ddedca44c171101d00b
Arun Ramachandran <arun.ramachandran6@wipro.com>
2019-10-17 22:45:18 +0100

    Added testing framework for constrictor prototype logic testing

--- salesTax.js
+++ salesTax.js
@@ -2,14 +2,20 @@
 let TaxConstructor = function (pList) {
     this.products = this.abstractProducts(pList);
     //this.classifiedProducts = this.getClassifiedProductsList(this.products);
-    this.total = this.calculateBillAmount(this.products);
-    this.salesTax = this.calculateSalesTax(this.products, this.total);
+    //this.total = this.calculateBillAmount(this.products);
+    //this.salesTax = this.calculateSalesTax(this.products, this.total);
 }
 
 TaxConstructor.prototype.abstractProducts = (pList) => {
     return [];
 }
 
-export const readProductsList = (productList) => {
+const createProductReceipt = (productList) => {
+    let purchaseReceipt = new TaxConstructor(productList);
     return [];
+}
+
+export {
+    TaxConstructor,
+    createProductReceipt
 }
--- salestax.spec.js
+++ salestax.spec.js
@@ -1,4 +1,4 @@
-import {readProductsList} from './salesTax';
+import {createProductReceipt, TaxConstructor} from './salesTax';
 
 describe('salesTax', () => {
     let inputSet;
@@ -7,7 +7,36 @@ describe('salesTax', () => {
         inputSet = ['1 book at 12.49', '1 music CD at 14.99', '1 chocolate bar at 0.85'];
     })
     it('should execute custom fn', () => {
-        expect(readProductsList(inputSet)).toEqual([]);
+        expect(createProductReceipt(inputSet)).toEqual([]);
+    })
+
+    it('should read the input & return the list of products', () => {
+        let expectedList = [
+            {
+                product: 'book',
+                qty: 1,
+                price: 12.49,
+                basicSalesTax: 0,
+                importDuty: 0.65
+            },
+            {
+                product: 'music CD',
+                qty: 1,
+                price: 14.99,
+                basicSalesTax: 1.50,
+                importDuty: 0.75
+            },
+            {
+                product: 'chocolate bar',
+                qty: 1,
+                price: 0.85,
+                basicSalesTax: 0,
+                importDuty: 0.05
+            }
+        ];
+
+        let productList = new TaxConstructor(inputSet);
+        expect(productList.products).toEqual([])
     })
 
 });
